fd49afc29cf9c64d761f9a9a46f8f0ed0e95e3d3
Iheb SAHLOUL <ihebsahloul@hotmail.fr>
2020-01-17 17:55:13 +0100

    implement user's position in map view

--- app/src/main/java/com/mtp/laboproject/view/ui/fragment/ProfilFragment.kt
+++ app/src/main/java/com/mtp/laboproject/view/ui/fragment/ProfilFragment.kt
@@ -1,45 +1,50 @@
 package com.mtp.laboproject.view.ui.fragment
 
 import android.Manifest
+import android.content.DialogInterface
 import android.content.Intent
-import android.graphics.Bitmap
-import androidx.lifecycle.ViewModelProviders
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.mtp.laboproject.view.factory.ProfilViewModelFactory
-import com.mtp.laboproject.view.ui.activity.AuthentificationActivity
-import com.mtp.laboproject.view.viewmodel.ProfilViewModel
-import kotlinx.android.synthetic.main.fragment_profil.*
-import org.jetbrains.anko.support.v4.intentFor
-import com.google.firebase.auth.UserProfileChangeRequest
-import android.net.Uri
+import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProviders
 import androidx.lifecycle.viewModelScope
 import com.bumptech.glide.Glide
-import com.mtp.laboproject.LaboApplication.Companion.auth
-import com.mtp.laboproject.R
-import com.mtp.laboproject.global.Constants.Variants.RESULT_LOAD_IMG
-import com.mtp.laboproject.global.bitmapToByte
-import com.mtp.laboproject.global.getRightAngleImage
-import permissions.dispatcher.*
-import com.mtp.laboproject.global.DebugLog
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import android.widget.LinearLayout
-import android.widget.EditText
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
-import org.jetbrains.anko.support.v4.act
+import com.google.firebase.auth.UserProfileChangeRequest
+import com.mtp.laboproject.LaboApplication.Companion.auth
+import com.mtp.laboproject.R
+import com.mtp.laboproject.global.Constants.Requests.MY_PERMISSIONS_REQUEST_LOCATION
+import com.mtp.laboproject.global.Constants.Variants.RESULT_LOAD_IMG
+import com.mtp.laboproject.global.DebugLog
+import com.mtp.laboproject.global.bitmapToByte
+import com.mtp.laboproject.global.getRightAngleImage
+import com.mtp.laboproject.view.factory.ProfilViewModelFactory
+import com.mtp.laboproject.view.ui.activity.AuthentificationActivity
+import com.mtp.laboproject.view.viewmodel.ProfilViewModel
+import kotlinx.android.synthetic.main.fragment_profil.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.jetbrains.anko.support.v4.intentFor
+import permissions.dispatcher.*
 
 @RuntimePermissions
-class ProfilFragment : BaseFragment() , OnMapReadyCallback {
+class ProfilFragment : BaseFragment() , OnMapReadyCallback , GoogleMap.OnMapClickListener, GoogleMap.OnInfoWindowClickListener, GoogleMap.OnMarkerClickListener{
 
 
     private lateinit var profilViewModel: ProfilViewModel
@@ -84,14 +89,14 @@ class ProfilFragment : BaseFragment() , OnMapReadyCallback {
             it.getMapAsync(this)
         }
     }
-    override fun onMapReady(googleMap: GoogleMap?) {
+    /*override fun onMapReady(googleMap: GoogleMap?) {
         mMap = googleMap!!
         // Add a marker in Sydney and move the camera
         val iff = LatLng(getString(R.string.iff_lat).toDouble(), getString(R.string.iff_lang).toDouble())
         //val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(iff).title("IFF"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(iff))
-    }
+    }*/
 
     private fun showDialogueWithEditText(){
         val input = EditText(context)
@@ -119,6 +124,48 @@ class ProfilFragment : BaseFragment() , OnMapReadyCallback {
             if(bitmap !=  null)
             Glide.with(context!!).load(bitmap?.let { bitmapToByte(it) }).into(profile_picture)
         }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+        mMap.uiSettings.isCompassEnabled = false
+        mMap.uiSettings.isZoomControlsEnabled = false
+        mMap.uiSettings.setAllGesturesEnabled(true)
+        if (ActivityCompat.checkSelfPermission(
+                activity!!,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) !== PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                activity!!,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) !== PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        // mMap.setMyLocationEnabled(true);
+        mMap.isMyLocationEnabled
+        mMap.setOnMapClickListener(this)
+        mMap.setOnInfoWindowClickListener(this)
+        mMap.setOnMarkerClickListener(this)
+
+        //Initialize Google Play Services
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(
+                    activity!!,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+                === PackageManager.PERMISSION_GRANTED
+            ) { //Location Permission already granted
+               // buildGoogleApiClient()
+                //  mMap.setMyLocationEnabled(true);
+            } else { //Request Location Permission
+                checkLocationPermission()
+            }
+        } else {
+          //  buildGoogleApiClient()
+            //  mMap.setMyLocationEnabled(true);
+        }
+        // setMarkerExample(mMap, 36.833670, 10.234702, "Car Dealer", R.drawable.car_marker); //TODO : eliminate example markers
     }
 
     private fun updateFireBaseUser(uri: Uri?, name: String?) {
@@ -206,6 +253,57 @@ class ProfilFragment : BaseFragment() , OnMapReadyCallback {
         private val TAG = ProfilFragment::class.toString()
     }
 
+    private fun isPermissionGiven(): Boolean{
+        return ActivityCompat.checkSelfPermission(activity!!, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(
+                activity!!,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            !== PackageManager.PERMISSION_GRANTED
+        ) { // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    activity!!,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            ) {
+                android.app.AlertDialog.Builder(activity)
+                    .setTitle(R.string.location_perm_title)
+                    .setMessage(R.string.location_perm_string)
+                    .setPositiveButton(R.string.ok,
+                        DialogInterface.OnClickListener { dialogInterface, i ->
+                            //Prompt the user once explanation has been shown
+                            ActivityCompat.requestPermissions(
+                                activity!!,
+                                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                                MY_PERMISSIONS_REQUEST_LOCATION
+                            )
+                        })
+                    .create()
+                    .show()
+            } else { // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(
+                    activity!!,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    MY_PERMISSIONS_REQUEST_LOCATION
+                )
+            }
+        }
+    }
+
+    override fun onMapClick(p0: LatLng?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onInfoWindowClick(p0: Marker?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onMarkerClick(p0: Marker?): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
 }
 
